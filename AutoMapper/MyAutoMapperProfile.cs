f29a269bde22afc73171ae17adc2d38875f86a55
Mitch <mitchchiles@gmail.com>
2021-04-20 18:39:32 -0400

    initial

---
+++ AutoMapper/MyAutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AutoMapper;
+using net_core_console_template.Models;
+
+namespace net_core_console_template.AutoMapper
+{
+    public class MyAutoMapperProfile : Profile
+    {
+        public MyAutoMapperProfile()
+        {
+            CreateMap<Foo, FooDto>();
+            // Use CreateMap... Etc.. here (Profile methods are the same as configuration methods)
+        }
+    }
+}
---
+++ Models/Foo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace net_core_console_template.Models
+{
+    public class Foo
+    {
+        
+    }
+
+    public class FooDto
+    {
+
+    }
+}
